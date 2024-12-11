4480cbdb21be4e0e9a22546f930458e47368e92a
fullstacksince2011 <fullstacksince2011>
2024-12-11 11:14:45 +0530

    basic

---
+++ 01basic-react/src/App.js
@@ -0,0 +1,12 @@
+import NewFile from "./newFile";
+
+function App() {
+  return (
+    <>
+      <h1>learning React</h1>
+      <NewFile />
+    </>
+  );
+}
+   
+export default App;
---
+++ 01basic-react/src/newFile.js
@@ -0,0 +1,10 @@
+function NewFile () {
+    return (
+        <>
+            <h2>New File Added</h2>
+            <p>lorem</p>
+        </>
+    )
+}
+
+export default NewFile
