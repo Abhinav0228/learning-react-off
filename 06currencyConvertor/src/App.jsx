3de586b82fed15ccaa054afc29975ac94a040f20
fullstacksince2011 <fullstacksince2011>
2025-01-03 19:05:45 +0530

    learn

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -1,8 +1,29 @@
 import { useState } from 'react'
+import { Input } from './components'
+import useCurrencyInfo from './Hooks/useCurrencyInfo'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("inr")
+  const [convertedAmount, setConvertedAmount] = useState(0)
+
+  const currencyInfo = useCurrencyInfo(from)
+
+  const options = Object.keys(currencyInfo)
+
+  const swap = () => {
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
+
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to])
+  }
 
   return (
     <>
@@ -11,4 +32,4 @@ function App() {
   )
 }
 
-export default App
+export default App  
